78b8b3d0bd0927d13875b614d51aa04949453084
Eric Lathrop <eric@ericlathrop.com>
2016-03-23 23:35:20 -0400

    Tabs to spaces

--- index.js
+++ index.js
@@ -1,96 +1,96 @@
 var mappings = require("./mappings.json");
 
 function getMapping(gamepadId, userAgent) {
-	return mappings.filter(function(mapping) {
-		return gamepadId.indexOf(mapping.id) !== -1 && userAgent.indexOf(mapping.userAgent) !== -1;
-	})[0] || mappings[0];
+  return mappings.filter(function(mapping) {
+    return gamepadId.indexOf(mapping.id) !== -1 && userAgent.indexOf(mapping.userAgent) !== -1;
+  })[0] || mappings[0];
 }
 
 function transformButton(mapping, gp, button, i) {
-	var mb = mapping.buttons[i] || { name: "button " + i };
-	gp.buttons[mb.name] = button.pressed;
-	if (mb.axis) {
-		if (button.pressed) {
-			gp.axes[mb.axis] = mb.axisValue;
-		} else if (gp.axes[mb.axis] === undefined) {
-			gp.axes[mb.axis] = 0;
-		}
-	}
-	return gp;
+  var mb = mapping.buttons[i] || { name: "button " + i };
+  gp.buttons[mb.name] = button.pressed;
+  if (mb.axis) {
+    if (button.pressed) {
+      gp.axes[mb.axis] = mb.axisValue;
+    } else if (gp.axes[mb.axis] === undefined) {
+      gp.axes[mb.axis] = 0;
+    }
+  }
+  return gp;
 }
 
 function scaleAxis(axis, scale) {
-	if (scale === "to positive") {
-		return (axis + 1.0) / 2.0;
-	}
-	if (scale === "to negative") {
-		return (axis + 1.0) / -2.0;
-	}
-	return axis;
+  if (scale === "to positive") {
+    return (axis + 1.0) / 2.0;
+  }
+  if (scale === "to negative") {
+    return (axis + 1.0) / -2.0;
+  }
+  return axis;
 }
 
 function transformAxis(mapping, threshold, gp, axis, i) {
-	var ma = mapping.axes[i] || { name: "axis " + i };
-	gp.axes[ma.name] = scaleAxis(axis, ma.scale);
-	if (ma.buttons) {
-		if (ma.buttons[0] !== null) {
-			gp.buttons[ma.buttons[0]] = axis < -threshold;
-		}
-		if (ma.buttons[1] !== null) {
-			gp.buttons[ma.buttons[1]] = axis > threshold;
-		}
-	}
-	return gp;
+  var ma = mapping.axes[i] || { name: "axis " + i };
+  gp.axes[ma.name] = scaleAxis(axis, ma.scale);
+  if (ma.buttons) {
+    if (ma.buttons[0] !== null) {
+      gp.buttons[ma.buttons[0]] = axis < -threshold;
+    }
+    if (ma.buttons[1] !== null) {
+      gp.buttons[ma.buttons[1]] = axis > threshold;
+    }
+  }
+  return gp;
 }
 
 function transformGamepad(threshold, gamepad) {
-	var gp = {
-		id: gamepad.id,
-		buttons: {},
-		axes: {}
-	};
-	var mapping = getMapping(gamepad.id, navigator.userAgent);
-	gp = gamepad.buttons.reduce(transformButton.bind(undefined, mapping), gp),
-	gp = gamepad.axes.reduce(transformAxis.bind(undefined, mapping, threshold), gp);
-	return gp;
+  var gp = {
+    id: gamepad.id,
+    buttons: {},
+    axes: {}
+  };
+  var mapping = getMapping(gamepad.id, navigator.userAgent);
+  gp = gamepad.buttons.reduce(transformButton.bind(undefined, mapping), gp),
+  gp = gamepad.axes.reduce(transformAxis.bind(undefined, mapping, threshold), gp);
+  return gp;
 }
 
 function isDefined(val) {
-	return val !== undefined;
+  return val !== undefined;
 }
 
 function Gamepad() {
-	this.threshold = 0.05;
-	this.gamepads = [];
+  this.threshold = 0.05;
+  this.gamepads = [];
 }
 Gamepad.prototype.update = function() {
-	// navigator.getGamepads() returns an array-like object, not an actual object
-	// so convert to an array so we can call map()
-	//
-	// WTF: webkit always returns 4 gamepads, so remove the undefined ones
-	var gamepads = Array.prototype.slice.call(navigator.getGamepads()).filter(isDefined);
-	this.gamepads = gamepads.map(transformGamepad.bind(undefined, this.threshold));
+  // navigator.getGamepads() returns an array-like object, not an actual object
+  // so convert to an array so we can call map()
+  //
+  // WTF: webkit always returns 4 gamepads, so remove the undefined ones
+  var gamepads = Array.prototype.slice.call(navigator.getGamepads()).filter(isDefined);
+  this.gamepads = gamepads.map(transformGamepad.bind(undefined, this.threshold));
 };
 Gamepad.prototype.axis = function(gamepad, axis) {
-	if (gamepad >= this.gamepads.length) {
-		return 0;
-	}
-	return this.gamepads[gamepad].axes[axis];
+  if (gamepad >= this.gamepads.length) {
+    return 0;
+  }
+  return this.gamepads[gamepad].axes[axis];
 };
 Gamepad.prototype.count = function() {
-	return this.gamepads.length;
+  return this.gamepads.length;
 }
 Gamepad.prototype.button = function(gamepad, button) {
-	if (gamepad >= this.gamepads.length) {
-		return false;
-	}
-	return this.gamepads[gamepad].buttons[button];
+  if (gamepad >= this.gamepads.length) {
+    return false;
+  }
+  return this.gamepads[gamepad].buttons[button];
 };
 Gamepad.prototype.name = function(gamepad) {
-	if (gamepad >= this.gamepads.length) {
-		return undefined;
-	}
-	return this.gamepads[gamepad].id;
+  if (gamepad >= this.gamepads.length) {
+    return undefined;
+  }
+  return this.gamepads[gamepad].id;
 };
 
 module.exports = Gamepad;
