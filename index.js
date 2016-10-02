9ef92abb6e1c2875335b00a60af716d613e8a161
Eric Lathrop <eric@ericlathrop.com>
2016-10-01 23:14:53 -0400

    Basically rewrite everything

--- index.js
+++ index.js
@@ -1,99 +1,43 @@
 var mappings = require("./mappings.json");
 
-function getMapping(gamepadId, userAgent) {
-  return mappings.filter(function(mapping) {
-    return gamepadId.indexOf(mapping.id) !== -1 && userAgent.indexOf(mapping.userAgent) !== -1;
-  })[0] || mappings[0];
-}
+window.addEventListener("gamepadconnected", function(e) {
+  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.", e.gamepad.index, e.gamepad.id, e.gamepad.buttons.length, e.gamepad.axes.length);
+  gamepads[e.gamepad.index] = new SingleGamepad(e.gamepad);
+});
 
-function transformButton(mapping, gp, button, i) {
-  var mb = mapping.buttons[i] || { name: "button " + i };
-  gp.buttons[mb.name] = button.pressed;
-  if (mb.axis) {
-    if (button.pressed) {
-      gp.axes[mb.axis] = mb.axisValue;
-    } else if (gp.axes[mb.axis] === undefined) {
-      gp.axes[mb.axis] = 0;
-    }
-  }
-  return gp;
-}
+window.addEventListener("gamepaddisconnected", function(e) {
+  console.log("Gamepad disconnected from index %d: %s", e.gamepad.index, e.gamepad.id);
+  gamepads[e.gamepad.index] = undefined;
+});
 
-function scaleAxis(axis, scale) {
-  if (scale === "to positive") {
-    return (axis + 1.0) / 2.0;
-  }
-  if (scale === "to negative") {
-    return (axis + 1.0) / -2.0;
-  }
-  return axis;
-}
+var gamepads = [];
+window.gamepads = gamepads;
+module.exports = gamepads;
 
-function transformAxis(mapping, threshold, gp, axis, i) {
-  var ma = mapping.axes[i] || { name: "axis " + i };
-  gp.axes[ma.name] = scaleAxis(axis, ma.scale);
-  if (ma.buttons) {
-    if (ma.buttons[0] !== null) {
-      gp.buttons[ma.buttons[0]] = axis < -threshold;
-    }
-    if (ma.buttons[1] !== null) {
-      gp.buttons[ma.buttons[1]] = axis > threshold;
-    }
-  }
-  return gp;
+function SingleGamepad(gp) {
+  this.gamepad = gp;
+  this.mapping = this.detectMapping();
 }
-
-function transformGamepad(threshold, gamepad) {
-  var gp = {
-    id: gamepad.id,
-    buttons: {},
-    axes: {}
-  };
-  var mapping = getMapping(gamepad.id, navigator.userAgent);
-  gp = gamepad.buttons.reduce(transformButton.bind(undefined, mapping), gp),
-  gp = gamepad.axes.reduce(transformAxis.bind(undefined, mapping, threshold), gp);
-  return gp;
-}
-
-function isDefined(val) {
-  return val !== undefined;
-}
-
-function Gamepad() {
-  this.threshold = 0.05;
-  this.gamepads = [];
-}
-Gamepad.prototype.update = function() {
-  if (typeof navigator.getGamepads !== "function") {
-    return;
-  }
-  // navigator.getGamepads() returns an array-like object, not an actual object
-  // so convert to an array so we can call map()
-  //
-  // WTF: webkit always returns 4 gamepads, so remove the undefined ones
-  var gamepads = Array.prototype.slice.call(navigator.getGamepads()).filter(isDefined);
-  this.gamepads = gamepads.map(transformGamepad.bind(undefined, this.threshold));
+SingleGamepad.prototype.detectMapping = function() {
+  return clone(mappings[0]);
 };
-Gamepad.prototype.axis = function(gamepad, axis) {
-  if (gamepad >= this.gamepads.length) {
+SingleGamepad.prototype.axis = function(name) {
+  var mapping = this.mapping.axes[name];
+  if (!mapping) {
     return 0;
   }
-  return this.gamepads[gamepad].axes[axis];
+  var index = mapping.index;
+  return this.gamepad.axes[index];
 };
-Gamepad.prototype.count = function() {
-  return this.gamepads.length;
-};
-Gamepad.prototype.button = function(gamepad, button) {
-  if (gamepad >= this.gamepads.length) {
+SingleGamepad.prototype.button = function(name) {
+  var mapping = this.mapping.buttons[name];
+  if (!mapping) {
     return false;
   }
-  return this.gamepads[gamepad].buttons[button];
-};
-Gamepad.prototype.name = function(gamepad) {
-  if (gamepad >= this.gamepads.length) {
-    return undefined;
-  }
-  return this.gamepads[gamepad].id;
+  var index = mapping.index;
+  return this.gamepad.buttons[index].pressed;
 };
 
-module.exports = Gamepad;
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
