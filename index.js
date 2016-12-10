1a07ad5bb67311fff4464137d51a23e3d2d3abcc
Eric Lathrop <eric@ericlathrop.com>
2016-12-10 18:39:19 -0500

    Add xbox360/chrome/linux xbone/chrome/mac

--- index.js
+++ index.js
@@ -16,11 +16,26 @@ module.exports = gamepads;
 
 function SingleGamepad(gp) {
   this.gamepad = gp;
-  this.mapping = this.detectMapping();
+  this.mapping = this.detectMapping(gp.id, navigator.userAgent);
 }
-SingleGamepad.prototype.detectMapping = function() {
+SingleGamepad.prototype.detectMapping = function(id, browser) {
+  for (var i = 0; i < mappings.length; i++) {
+    if (isCompatible(mappings[i], id, browser)) {
+      console.log("found mapping", mappings[i].name, "for", id, "on", browser);
+      return clone(mappings[i]);
+    }
+  }
   return clone(mappings[0]);
 };
+function isCompatible(mapping, id, browser) {
+  for (var i = 0; i < mapping.supported.length; i++) {
+    var supported = mapping.supported[i];
+
+    return id.indexOf(supported.id) !== -1
+      && browser.indexOf(supported.os) !== -1
+      && browser.indexOf(browser) !== -1;
+  };
+}
 SingleGamepad.prototype.axis = function(name) {
   var mapping = this.mapping.axes[name];
   if (!mapping) {
