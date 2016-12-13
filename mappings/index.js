cb2dc14fecea48382ffce3bcf74b7a154aa4bfc4
Eric Lathrop <eric@ericlathrop.com>
2016-12-12 21:05:03 -0500

    Add more mappings

--- mappings/index.js
+++ mappings/index.js
@@ -1,14 +1,23 @@
 module.exports = [
   require("./default.json"),
+
   require("./logitechf310-directinput-chrome-linux.json"),
   require("./logitechf310-directinput-chrome-osx.json"),
   require("./logitechf310-directinput-chrome-windows.json"),
+  require("./logitechf310-directinput-ff-osx.json"),
+
   require("./logitechf310-xinput-chrome-linux.json"),
   require("./logitechf310-xinput-ff-linux.json"),
-  require("./ps4-chrome-windows.json"),
+
+  require("./ps4-chrome-linux.json"),
   require("./ps4-chrome-osx.json"),
+  require("./ps4-chrome-windows.json"),
+  require("./ps4-ff-linux.json"),
+  require("./ps4-ff-osx.json"),
+
   require("./xbone-chrome-linux.json"),
   require("./xbone-chrome-osx-linux.json"),
+
   require("./xbox360-chrome-osx.json"),
   require("./xbox360-chrome-windows.json"),
   require("./xbox360-ff-windows.json"),
