3da61e6a0cebec0fa8054cbdab2219507a7a4bfe
Eric Lathrop <eric@ericlathrop.com>
2016-12-22 16:48:02 -0500

    Combine F310 Chrome mappings for Win/OSX

--- mappings/index.js
+++ mappings/index.js
@@ -2,8 +2,7 @@ module.exports = [
   require("./default.json"),
 
   require("./logitechf310-directinput-chrome-linux.json"),
-  require("./logitechf310-directinput-chrome-osx.json"),
-  require("./logitechf310-directinput-chrome-windows.json"),
+  require("./logitechf310-directinput-chrome-windows-osx.json"),
   require("./logitechf310-directinput-ff-linux.json"),
   require("./logitechf310-directinput-ff-osx.json"),
   require("./logitechf310-directinput-ff-windows.json"),
